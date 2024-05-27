59179edc3062a9524e54d2cacd6bbf4023ff79ae
Richard Michalek <micr02@vse.cz>
2024-05-27 12:42:31 +0200

    adding images

--- src/components/Car.jsx
+++ src/components/Car.jsx
@@ -1,11 +1,24 @@
 const Car = (props) => {
+  const { brand, model, location, id, image, price, color } = props;
+
   return (
-    <div>
-      <h2>{props.brand}</h2>
-      <h3>{props.model}</h3>
-      <h3>{props.price}</h3>
-      <h3>{props.color}</h3>
-    </div>
+    <a href={`/details/${id}`}>
+      <div className="flex w-full rounded-md border p-2 shadow-sm hover:bg-slate-100">
+        <div>
+          <img
+            src={image}
+            alt={model}
+            className="size-12 flex-none rounded-full bg-gray-50"
+          />
+        </div>
+        <div className="pl-5 text-base">
+          <h1 className="text-xl">
+            {brand} {model}
+          </h1>
+          <h2>{`${color} - ${location} - ${price} CZK`}</h2>
+        </div>
+      </div>
+    </a>
   );
 };
 
--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -4,7 +4,7 @@ const CarList = (props) => {
   return (
     <div>
       {props.cars.map((car) => (
-        <Car brand={car.brand} model={car.model} color={car.color} />
+        <Car brand={car.brand} model={car.model} color={car.color} year={car.year} id={car.id} image={car.image}/>
       ))}
     </div>
   );
